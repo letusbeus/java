ed28628ceea96638b661cda2bf8eea14757aea9b
letusbeus <letusbeus@mail.ru>
2022-10-06 12:10:10 +0300

    Added 2 methods to Task Two

--- src/ru/letusbeus/JavaHomeworkThree/Main.java
+++ src/ru/letusbeus/JavaHomeworkThree/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(TaskOne.oddNumsOnly(new ArrayList<>(Arrays.asList(8, 0, -3, 5, 6, 9, 8, -4, 2, -99, 43))));
         System.out.println(TaskTwo.requiredNums(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))));
+        System.out.println(TaskTwo.requiredNumsV2(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))));
+        System.out.println(TaskTwo.requiredNumsV3(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))));
         System.out.println(TaskThree.sortedArray(new int[]{})); // проверка на пустом массиве
         System.out.println(TaskThree.sortedArray(new int[]{999})); // проверка на массиве с одним элементом
         System.out.println(TaskThree.sortedArray(new int[]{8, 0, -3, 5, 6, 9, 8, -4, 2, -99, 43}));
--- src/ru/letusbeus/JavaHomeworkThree/TaskTwo.java
+++ src/ru/letusbeus/JavaHomeworkThree/TaskTwo.java
@@ -18,4 +18,26 @@ public class TaskTwo {
 //             "\nMax value: " + Collections.max(nums) +
 //             "\nAverage value: " + nums.stream().mapToDouble(i -> i).sum() / nums.size();
     }
+
+    public static String requiredNumsV2(ArrayList<Integer> nums) {
+        int minNum = Collections.min(nums);
+        int maxNum = Collections.max(nums);
+        double average = sumOfNums(nums) / nums.size();
+        return "\nTask Two\nGiven list: " + nums + "\nMin: " + minNum + "\nMax: " + maxNum + "\nAverage: " + average;
+    }
+    public static double sumOfNums(ArrayList<Integer> nums) {
+        int sum = 0;
+        for (Integer num : nums) sum += num;
+        return sum;
+    }
+    public static String requiredNumsV3(ArrayList<Integer> nums) {
+        int minVal = Collections.min(nums);
+        int maxVal = Collections.max(nums);
+        int[] arrOfNums = new int[nums.size()];
+        for (int i = 0; i < arrOfNums.length; i++){
+            arrOfNums[i] = nums.get(i);
+        }
+        double averageVal = (double) Arrays.stream(arrOfNums).sum() / arrOfNums.length;
+        return "\nTask Two\nGiven list: " + nums + "\nMin: " + minVal + "\nMax: " + maxVal + "\nAverage: " + averageVal;
+    }
 }
